4b9ddaa4ccb2958b193537c465bb0d3e535c9c16
Depechie <glenn.versweyveld@gmail.com>
2017-02-19 20:46:20 +0100

    Added the custom renderer version

---
+++ iOS/Renderers/ClearEntryRenderer.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormsNativeDemo.iOS.Renderers;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(ClearEntryRenderer))]
+namespace XamarinFormsNativeDemo.iOS.Renderers
+{
+    public class ClearEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if(Control != null)
+                //Control.ClearButtonMode = UIKit.UITextFieldViewMode.WhileEditing;
+        }
+    }
+}
